97ffaa571f81d51d36eb1a9c2819a3bde9efa8f6
Emon <150001421+doodleEmon@users.noreply.github.com>
2024-12-12 02:52:55 +0600

    product schema done

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, connect } from "mongoose";
+import { Product } from "./product.interface";
+
+const VariantSchema = new Schema({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const InventorySchema = new Schema({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const userSchema = new Schema<Product>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [VariantSchema], required: true },
+  inventory: { type: InventorySchema, required: true },
+});
