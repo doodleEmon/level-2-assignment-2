ded35201c0202bb32b31d7d6f70e02429320a09c
Emon <150001421+doodleEmon@users.noreply.github.com>
2024-12-12 01:25:10 +0600

    all setup done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,28 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-unused-expressions": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    }
+  },
+  {
+    globals: {
+      "process": "readonly"
+    }
+  },
+  {
+    ignores: [".node_modules/*, .dist"]
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express from 'express'
-import cors from 'cors'
-const app = express()
+import express from "express";
+import cors from "cors";
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-app.get('/', (req, res) => {
-  const a = 10
-  res.send(a)
-})
+app.get("/", (req, res) => {
+  const a = 10;
+  res.send(a);
+});
 
 export default app;
