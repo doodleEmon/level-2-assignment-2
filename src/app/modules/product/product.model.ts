3c37bcc3403055ffba941e330e16fa8d407ccfb4
Emon <150001421+doodleEmon@users.noreply.github.com>
2024-12-12 14:10:54 +0600

    router, controller and service created successfully for post product

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,22 +1,24 @@
 import { Schema, model, connect } from "mongoose";
-import { Product } from "./product.interface";
+import { Inventory, Product, Variant } from "./product.interface";
 
-const VariantSchema = new Schema({
+const variantSchema = new Schema<Variant>({
   type: { type: String, required: true },
   value: { type: String, required: true },
 });
 
-const InventorySchema = new Schema({
+const inventorySchema = new Schema<Inventory>({
   quantity: { type: Number, required: true },
   inStock: { type: Boolean, required: true },
 });
 
-const userSchema = new Schema<Product>({
+const productSchema = new Schema<Product>({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
   tags: { type: [String], required: true },
-  variants: { type: [VariantSchema], required: true },
-  inventory: { type: InventorySchema, required: true },
+  variants: { type: [variantSchema], required: true },
+  inventory: { type: inventorySchema, required: true },
 });
+
+export const ProductModel = model<Product>("Product", productSchema);
