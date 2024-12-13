4a47ee39925fe771b3642fd916e8790418be3625
Emon <150001421+doodleEmon@users.noreply.github.com>
2024-12-13 16:42:29 +0600

    Route not found: error handler done

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { ProductRoutes } from "./app/modules/product/product.routes";
 import { OrderRoutes } from "./app/modules/order/order.routes";
@@ -9,5 +9,17 @@ app.use(cors());
 
 app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
+
+// 404 Route Not Found Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Route not found!" });
+});
+
+// General Error Handling Middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
